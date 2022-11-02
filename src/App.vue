03133dfec14f7e27921629aabcde25a0aa1f86f8
idooru <shere1765@gmail.com>
2022-11-02 18:50:10 +0900

    [수정] 반복문을 사용하여 상품 정보 표시

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,109 @@
 <template>
+  <div id="black-bg" v-if="isModalOpened === true">
+    <div id="white-bg">
+      <button @click="closeModal">X</button>
+    </div>
+  </div>
   <div class="menu">
     <a v-for="menu in menus" :key="menu"> {{ menu }} </a>
   </div>
-  <h1 :style="element.myStyle">원룸샵</h1>
+  <h1 class="title">원룸샵</h1>
   <div id="products">
-    <!-- <div v-for="(product, idx) in buried.products" :key="(product, idx)">
-      <h3>{{ product }}</h3>
-      <p>{{ buried.price[idx] }}</p>
-    </div> -->
-    <div>
-      <h3>{{ buried.products[0] }}</h3>
-      <p>50만원</p>
+    <div v-for="(roomInfo, idx) in roomInfos" :key="(roomInfo, idx)">
+      <img :src="roomInfos[idx].image" class="room-image" />
+      <h3 @click="openModal(idx)">{{ roomInfos[idx].title }}</h3>
+      <p>{{ roomInfos[idx].price }} 원</p>
+      <p>{{ roomInfo.content }}</p>
       <div class="report">
-        <button>허위매몰신고</button>
-        <span>신고수 : 0</span>
-      </div>
-    </div>
-    <div>
-      <h3>{{ buried.products[1] }}</h3>
-      <p>60만원</p>
-      <div class="report">
-        <button>허위매몰신고</button>
-        <span>신고수 : 0</span>
-      </div>
-    </div>
-    <div>
-      <h3>{{ buried.products[2] }}</h3>
-      <p>70만원</p>
-      <div class="report">
-        <button>허위매몰신고</button>
-        <span>신고수 : 0</span>
+        <button @click="increaseReportCount(idx)">허위매몰신고</button>
+        <span>신고수 : {{ roomInfos[idx].countOfReport }}</span>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import roomInfos from "./data.js";
+
 export default {
   name: "App",
   data() {
     return {
+      roomInfos,
+      isModalOpened: false,
       menus: ["Home", "Product", "About"],
-      buried: {
-        products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
-        price: [50, 60, 70],
-      },
-      element: { myStyle: "color: red" },
     };
+  },
+  methods: {
+    increaseReportCount(idx) {
+      this.roomInfos[idx].countOfReport++;
+    },
+    openModal(idx) {
+      this.isModalOpened = true;
+
+      const detailPageTitle = document.createElement("h3");
+      const detailPageContents = document.createElement("p");
+
+      detailPageTitle.innerText = this.roomInfos[idx].title;
+      detailPageContents.innerText = this.roomInfos[idx].content;
+
+      console.log(detailPageTitle.innerText);
+      console.log(detailPageContents.innerText);
+
+      const whiteBG = document.querySelector("#white-bg");
+      console.log(whiteBG);
+      // whiteBG.appendChild(detailPageTitle);
+      // whiteBG.appendChild(detailPageContents);
+    },
+    closeModal() {
+      this.isModalOpened = false;
+    },
   },
   components: {},
 };
 </script>
 
 <style>
+body {
+  margin: 0px;
+}
+
+div {
+  box-sizing: border-box;
+}
+
+#black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding-top: 70px;
+  padding-bottom: 20px;
+  padding-left: 120px;
+  padding-right: 120px;
+}
+
+#white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
+#white-bg > button {
+  position: absolute;
+  top: 70px;
+  right: 140px;
+  color: red;
+  font-weight: 650;
+  border: none;
+  background-color: rgb(218, 218, 218);
+  padding-top: 8px;
+  padding-bottom: 8px;
+  padding-left: 26px;
+  padding-right: 26px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -61,16 +112,14 @@ export default {
   color: #1567b9;
 }
 
-/* h1 {
-  text-decoration-style: dotted;
-  font-size: 50px;
-  font-weight: 900;
-  color: #122436;
-  text-shadow: 2px 2px 10px rebeccapurple;
-} */
+.title {
+  color: crimson;
+  font-size: 65px;
+}
 
 .menu {
   background-color: darkslateblue;
+  /* margin-top: 25px; */
   margin-left: 300px;
   margin-right: 300px;
   margin-bottom: 50px;
@@ -81,6 +130,13 @@ export default {
 .menu a {
   color: whitesmoke;
   padding: 10px;
+}
+
+.room-image {
+  border: 3px solid rgb(15, 71, 145);
+  border-radius: 15px;
+  width: 50%;
+  margin: 40px;
 }
 
 .report {
@@ -102,6 +158,10 @@ export default {
 
 .report > span {
   margin-left: 10px;
+}
+
+.last {
+  padding-bottom: 18px;
 }
 
 #products {
